0ac3ac55369d51bb78fda89d516416c5042be892
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2015-10-30 10:18:19 +0900

    add chef

---
+++ chef/cookbooks/local-accounts/recipes/fujiwara.rb
@@ -0,0 +1,30 @@
+user "fujiwara" do
+  password "$6$K0ZhMyG4$Feu6sh.kOMl2KMS6R4maLTkDRbbqceH22tFGVxwNNljB7bxj8jpZpksPlEXM3Q.8Yy18ljKSIl/x/4zVtzNca."
+  home "/home/fujiwara"
+  supports :manage_home => true
+  shell "/bin/bash"
+end
+
+directory "/home/fujiwara/.ssh" do
+  mode 0700
+  owner "fujiwara"
+end
+
+remote_file "/home/fujiwara/.ssh/authorized_keys" do
+  mode 0600
+  owner "fujiwara"
+  source "https://github.com/fujiwara.keys"
+end
+
+file "/home/fujiwara/.screenrc" do
+  owner "fujiwara"
+  mode 0644
+  content "escape ^z^z\n"
+end
+
+execute "setup GOPATH" do
+  user "fujiwara"
+  cwd "/home/fujiwara"
+  command "echo 'export GOPATH=/home/fujiwara' >> .bash_profile"
+  not_if "grep GOPATH .bash_profile"
+end
